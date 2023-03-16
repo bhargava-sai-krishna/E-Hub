529a02434a7c8ed726afc7f071ad8a4488f3845b
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2023-03-16 21:58:21 +0530

    Add files via upload

---
+++ SwingProject/src/GUI/secret.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class secret {
+	public String uname="postgres";
+	public String password="sbskln2412S";
+	public String url="jdbc:postgresql://localhost:5432/javaProject";
+}
